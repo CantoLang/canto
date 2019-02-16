e99da703dfc5a57274ca5aadeeda210c640c39ce
sthippo <mash@brooklyndigital.net>
2019-02-16 14:44:03 -0500

    make it workable in original canto directory

--- install.sh
+++ install.sh
@@ -8,13 +8,13 @@ EXECUTABLE_NAME=$(basename $DIR)
 
 pushd $DIR >/dev/null
 
-echo "Extracting jars..."
-jar xf lib/canto.jar lib/jetty-all.jar lib/servlet-api-3.1.jar
-
 echo "Extracting scripts..."
 jar xf lib/canto.jar bin/cantoscript.sh bin/cantoserver.sh bin/generate_service.sh
 jar xf lib/canto.jar canto.sh canto_site.sh
-mv canto_site.sh $EXECUTABLE_NAME.sh
+if [[ "$EXECUTABLE_NAME" != "canto" ]]; then
+    mv canto_site.sh $EXECUTABLE_NAME.sh
+fi
+
 chmod +x bin/*.sh
 chmod +x *.sh
 
@@ -25,11 +25,17 @@ if [[ -f /usr/local/bin/$EXECUTABLE_NAME ]]; then
 elif [[ $EUID -eq 0 ]]; then
     echo "Setting up symlinks..."
     ln -s $DIR/$EXECUTABLE_NAME.sh /usr/local/bin/$EXECUTABLE_NAME
+    if [[ "$EXECUTABLE_NAME" != "canto" ]]; then
+        ln -s $DIR/canto.sh /usr/local/bin/canto
+    fi
     echo
     echo "Installation complete. Type " $EXECUTABLE_NAME " to run."
 else
     echo "Enter password to set up symlinks, or CTRL-C to exit."
     sudo ln -s $DIR/$EXECUTABLE_NAME.sh /usr/local/bin/$EXECUTABLE_NAME
+    if [[ "$EXECUTABLE_NAME" != "canto" ]]; then
+        sudo ln -s $DIR/canto.sh /usr/local/bin/canto
+    fi
     echo
     echo "Installation complete. Type " $EXECUTABLE_NAME " to run."
 fi
